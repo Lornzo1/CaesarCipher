39c4edac7f3464ee07283a28f3d2a0ad178b5819
Lorenzo Usai <UsaiL@StPaulsSchool.org.uk>
2020-10-05 13:09:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+namespace Trial
+{
+    class Program
+    {
+ /*       static string Coder(string a, int s)
+        {
+            a = a.ToUpper();
+            string a2 = "";
+            foreach (char i in a)
+            {
+                if (i == ' ') { a2 += " "; }
+                else if (90 - i - s < 0) { a2 += (Char)(-26 + int.Parse(i.ToString()) + s); }
+                else if (i + s < 65) { a2 += (Char)(26 + int.Parse(i.ToString()) + s); }
+                else { a2 += (Char)(i + s); }
+            }
+            return a2;
+        }
+ */
+        static string Coder(string a, int s)
+        {
+            string a2 = "";
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(a);
+            foreach (int i in a)
+            {
+                if (i == ' ') { a2 += " "; }
+                else if (90 - i - s < 0) { a2 += (Char)(-26 + i + s); }
+                else if (i + s < 65) { a2 += (Char)(26 + i + s); }
+                else { a2 += (Char)(i + s); }
+            }
+            return a2;
+        }
+        static string DeCoder(string a, int s)
+        {
+            return Coder(a, -s);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DeCoder(Coder("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", 3),3));
+        }
+    }
+}
